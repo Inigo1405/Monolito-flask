e8e3ee9c3388aa458a7739da710601fb77338b36
antonio-espinoza <joseantonioesp9@gmail.com>
2026-01-28 16:06:57 -0600

    app

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 from flask import Flask, render_template
-app = Flask(__name__)
-
-
-=======
 import sqlite3
-from flask import Flask, render_template# from db import db
-
 
 app = Flask(__name__)
-
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
@@ -17,7 +9,6 @@ def get_db_connection():
 
 
 
->>>>>>> 92f3c30c6db16f79696192ef415d97e344363b15
 @app.route('/')
 def base():
     conn = get_db_connection()
@@ -35,15 +26,12 @@ def login():
 def form():
     return render_template('form.html', name="Usuario")
 
-<<<<<<< HEAD
 @app.post('/products')
 def products():
     return render_template('products.html', name="Usuario")
 
 if __name__ == '__main__':
     app.run(debug=True)
-=======
 
 if __name__ == '__name__':
     app.run(debug = True)
->>>>>>> 92f3c30c6db16f79696192ef415d97e344363b15
